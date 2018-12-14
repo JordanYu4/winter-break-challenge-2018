97cdd6f3fd1d36ecf08bedfe81ca9a950674a968
Jordan Yu <jordan.yu@punchh.com>
2018-12-14 15:20:03 -0800

    Clean up comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  # get 'challenges', to: 'static_pages#challenges'
   get 'sharings', to: 'static_pages#sharings'
   get 'submit_post', to: 'static_pages#submit_post'
-  # get 'stats', to: 'static_pages#stats'
-  resources :comments, only: [:create]
-  resources :comment
-  resources :sg
-  resources :evangelism
-  resources :ser
-  resources :eq
+  resources :posts, only: [:create]
+  resources :posts
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
