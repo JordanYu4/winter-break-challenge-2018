5c0c895d17c0929595d54c9644491d6b53bce41d
Jordan Yu <jordan.yu@punchh.com>
2018-12-14 13:17:38 -0800

    Minimize seeds to example set

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,33 +22,6 @@ Mission.create!(name: '10 Days of Gratitude Journal (10 entries/day)', points: 5
 Mission.create!(name: 'Host Dinner for Your Family and/or Friends)', points: 60, category: ser)
 
 
-# evangelism
-Mission.create!(name: 'Share the Gospel and/or your testimony with a non-Christian friend or family member', points: 40, category: ev)
-Mission.create!(name: 'Bring a non-Christian friend or family member to church!', points: 50, category: ev)
-Mission.create!(name: 'Prepare a special talk or presentation on a Christian topic (can do this with your friends)', points: 50, category: ev)
-Mission.create!(name: 'Read a Christian book with a non Christian friend or family member', points: 60, category: ev)
-Mission.create!(name: 'Give a special talk or presentation on a Christian topic (can do this with your friends)', points: 100, category: ev)
-Mission.create!(name: 'Lead a bible study with a non-Christian friend or family member', points: 100, category: ev)
-
-
-# spiritual growth
-Mission.create!(name: 'Read Bible (20 Chapters)', points: 20, category: sg)
-Mission.create!(name: 'Memorize a Psalm', points: 25, category: sg)
-Mission.create!(name: 'Video Chat with a peer of LG: Share prayer requests or DT', points: 50, category: sg)
-Mission.create!(name: '10 consecutive days of DT', points: 50, category: sg)
-Mission.create!(name: 'Outline all 4 Gospels', points: 50, category: sg)
-Mission.create!(name: 'Read a Christian book: Write a blurb about it!', points: 50, category: sg)
-Mission.create!(name: '10 Min Prayer Daily for a week', points: 50, category: sg)
-
-
-
-# physical equipping
-Mission.create!(name: '35 push-ups / day', points: 5, category: equ)
-Mission.create!(name: 'GO running! 10 pts / 1 mile', points: 10, category: equ)
-Mission.create!(name: 'Join 7 minute workout app, add your LG and do a workout together (in person or via video chat)', points: 25, category: equ)
-Mission.create!(name: '5 Day Social/Video Media Fast ', points: 50, category: equ)
-Mission.create!(name: '1,000 Pushups (Honor System)', points: 70, category: equ)
-
 
 
 
