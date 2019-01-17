141f19b1e90c0f74f4dbd6411fd8d5a4aa7dc7e7
Jordan Yu <jordan.yu@punchh.com>
2019-01-16 19:37:23 -0800

    Comment on module naming

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Changing module name messes up deploy - investigate if time allows 
 module KoinjrWinterchallenge
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
